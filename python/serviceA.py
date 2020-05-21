f841806a7f1146094cafafdb1bd4d5c94bfde957
sforeman <sforeman@redhat.com>
2020-05-21 12:27:25 -0700

    add basic python application code for testing service mesh

---
+++ python/serviceA.py
@@ -0,0 +1,6 @@
+#service A
+import time
+import random
+while(True):
+  print("hello world, from service A!")
+  time.sleep(random.randint(2, 8))
---
+++ python/serviceB.py
@@ -0,0 +1,6 @@
+#service B
+import time
+import random
+while(True):
+  print("hello world, from service B!")
+  time.sleep(random.randint(2, 8))
